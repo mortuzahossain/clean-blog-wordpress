6c277369d71fdce19ca8227436540af70de3fa1a
mortuzahossain <mortuzahossain1997@gmail.com>
2017-11-07 16:45:34 +0600

    Done solving responcive issue

--- inc/js-css.php
+++ inc/js-css.php
@@ -9,7 +9,7 @@
             wp_enqueue_style( 'font-Sans', 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800', array(), '1.0.0', 'all' );
             wp_enqueue_style( 'clean-blog', BASE.'/css/clean-blog.css', array(), '1.0.0', 'all' );
             // For JS
-            wp_enqueue_script('bootstrap', BASE.'/vendor/bootstrap/js/bootstrap.min.js', array('jquery'), '1.0.0', true);
+            wp_enqueue_script('bootstrap', BASE.'/vendor/bootstrap/js/bootstrap.bundle.js', array('jquery'), '1.0.0', true);
             wp_enqueue_script( 'clean-blog', BASE.'/js/clean-blog.min.js', array('jquery'), '1.0.0', true );
 
         }
